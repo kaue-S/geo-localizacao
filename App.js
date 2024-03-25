526a6f048d63dd1add959bddd792f4571f13583f
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-03-25 09:03:09 -0300

    comit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+
+    // Define o zoom do mapa. Quanto menor o valor, maior o zoom do mapa.
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView
+          mapType="hybrid"
+          style={estilos.mapa}
+          initialRegion={regiaoInicialMapa}
+          userInterfaceStyle="dark"
+        />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  mapa: {
+    width: "100%",
+    height: "100%",
   },
 });
